cedc8740aa9a198dc35c3467917e677f1a82a968
Ryan DiMascio <ryan.dimascio@gmail.com>
2022-07-28 01:15:25 -0700

    fix: auto update bug (#40)
    feat: add plugin info to updater
    
    feat: use release-please to generate changelog

---
+++ README.md
@@ -0,0 +1,9 @@
+Writer's Blocks is a suite of authoring tools built for the Gutenberg editor. Write with more power and clarity, cut dead weight, and make your content more accessible, inclusive, and readable.
+
+It contains the following writing style checks:
+
+* **Spelling** - Catch spelling mistakes
+* **Simplify** - Check for simpler alternatives
+* **Intensity** - Avoid weak and mitigating wording
+* **Assuming** - Omit unhelpful phrases such as 'just', 'simply' or 'obviously'
+* **Passive voice** - Make your writing more assertive
--- config.php
+++ config.php
@@ -5,12 +5,14 @@
  * @package HoltInternationalPlugin
  */
 
-wb_plugin_define( 'WRITERS_BLOCKS_VERSION', '1.0.0' );
+wb_plugin_define( 'WRITERS_BLOCKS_VERSION', '0.1.1' );
+wb_plugin_define( 'WRITERS_BLOCKS_NAME', 'Writer\'s Blocks' );
 wb_plugin_define( 'WRITERS_BLOCKS_REQUIRES_WP_VERSION', '5.3' );
 wb_plugin_define( 'WRITERS_BLOCKS_COMPATIBLE_WP_VERSION', '5.9.3' );
 wb_plugin_define( 'WRITERS_BLOCKS_REQUIRES_PHP_VERSION', '7.0' );
 wb_plugin_define( 'WRITERS_BLOCKS_TESTED_WP_VERSION', '6.0.1' );
 wb_plugin_define( 'WRITERS_BLOCKS_URL', plugin_dir_url( __FILE__ ) );
 wb_plugin_define( 'WRITERS_BLOCKS_PATH', plugin_dir_path( __FILE__ ) );
+wb_plugin_define( 'WRITERS_BLOCKS_SITE_URL', 'https://usewritersblocks.com' );
 wb_plugin_define( 'WRITERS_BLOCKS_API_URL', 'https://api.usewritersblocks.com/wp-json/lsq/v1' );
 wb_plugin_define( 'WRITERS_BLOCKS_PATH_INC', WRITERS_BLOCKS_PATH . 'includes/' );
